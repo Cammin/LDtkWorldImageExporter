c23067a85a8fea4da14fbef729c509d188125c00
Cameron <cameo221@gmail.com>
2023-08-07 22:27:49 -0700

    add test and json data, write more window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,52 @@
-﻿namespace WorldImageMerger
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Win32;
+
+namespace WorldImageMerger
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [SuppressMessage("ReSharper", "LocalizableElement")]
+    public partial class MainWindow
     {
-      InitializeComponent();
+        public string WorkingDirectory;
+        
+        public string ChosenPath;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.WriteLine("Start app");
+
+            ExportButton.IsEnabled = false;
+            
+            WorkingDirectory = Environment.CurrentDirectory;
+            
+            PickButton.Click += (sender, args) =>
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.InitialDirectory = ChosenPath ?? WorkingDirectory;
+                dialog.Title = "Pick LDtk project";
+                dialog.Filter = "LDtk project (*.ldtk, *.json)|*.ldtk;*.json|All files (*.*)|*.*";
+                
+                if (dialog.ShowDialog().GetValueOrDefault())
+                {
+                    ChosenPath = dialog.FileName;
+                    Path.Content = ChosenPath;
+                    Console.WriteLine($"Picked. \"{ChosenPath}\"");
+                }
+                
+                ExportButton.IsEnabled = !string.IsNullOrEmpty(ChosenPath);
+            };
+
+            ExportButton.Click += (sender, args) =>
+            {
+                Console.WriteLine($"Export! \"{ChosenPath}\"");
+                
+                
+            };
+        }
     }
-  }
 }
